b3e3478d90fa923cd37e48a48d3a7e269661eb29
Keanan <keananwork21@gmail.com>
2025-04-22 09:29:18 +0200

    ha

---
+++ script.js
@@ -0,0 +1,10 @@
+// hamburger menu
+
+const hamMenu = document.querySelector('.hamburger-menu')
+
+const offScreenMenu = document.querySelector('.off-screen-menu')
+
+hamMenu.addEventListener('click', ()=>{
+    hamMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active')
+})
